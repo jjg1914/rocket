a90f38059a85abcdb3111b7282107b26084d3c86
John J. Glynn IV <jjg1914@rit.edu>
2017-10-21 13:30:14 -0400

    shrink spike hit-box

--- src/entities/spikes-entity.ts
+++ src/entities/spikes-entity.ts
@@ -4,13 +4,22 @@ import {
   ResolutionEvent,
   SimpleEntityConfig,
   SimpleEntity,
+  snap,
 } from "mu-engine";
 
 export class SpikesEntity extends SimpleEntity {
   constructor(config: Partial<SimpleEntityConfig>) {
     super(merge({
-      render: { sprite: "spikes.json", spriteFrame: 0 },
-    }, config));
+      render: {
+        sprite: "spikes.json",
+        spriteFrame: 0,
+        transform: [ 1, 0, -1, 0, 1, -4 ],
+      },
+    }, config, {
+      position: { width: 14, height: 12 },
+    }));
+
+    snap(this, "middle", "bottom", 16, 16);
 
     this.on("collision", (ev: ResolutionEvent) => {
       ev.target.send("die");
